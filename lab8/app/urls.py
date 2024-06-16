6738e637e1ad36073086902e326c0fdb89b57517
Anthony <acahuib@unsa.edu.pe>
2024-06-15 22:16:51 -0500

    Se agrega a la views.py la funcion para enviar correos

--- lab8/app/urls.py
+++ lab8/app/urls.py
@@ -1,9 +1,10 @@
 # lab8/app/urls.py
 
 from django.urls import path
-from .views import generate_pdf
+from .views import generate_pdf, send_test_email
 
 urlpatterns = [
     path('generate_pdf/', generate_pdf, name='generate_pdf'),
+    path('send_test_email/', send_test_email, name='send_test_email'),
 ]
 
--- lab8/app/views.py
+++ lab8/app/views.py
@@ -1,10 +1,25 @@
-# lab8/app/views.py
+# app/views.py
 
 from django.shortcuts import render
 from django.http import HttpResponse
 from weasyprint import HTML
 from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Framework, Language
+
+def send_test_email(request):
+    # Enviar correo electrónico
+    subject = "Test Email"
+    message = "This is a test email from Django."
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ["cahuianthony6@gmail.com"]
+    
+    try:
+        send_mail(subject, message, email_from, recipient_list)
+        return HttpResponse("Test email sent successfully.")
+    except Exception as e:
+        return HttpResponse(f"An error occurred: {e}")
 
 def generate_pdf(request):
     languages = Language.objects.all()
@@ -25,4 +40,3 @@ def generate_pdf(request):
     response['Content-Disposition'] = 'inline; filename="report.pdf"'
 
     return response
-
